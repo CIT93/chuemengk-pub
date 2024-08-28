7d795c0173ba0f048902010958aebb10d5878de5
Chuemeng Kue <chuek125@gmail.com>
2024-08-27 18:49:24 -0700

    Week 3 Attendance

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,26 @@
-console.log('Hello World')
+console.log('Hello from inside the main.js file');
+
+//let myVar;
+
+const myVar = -43;
+const myVarType = typeof(myVar);
+
+console.log("myVarType" + myVarType);
+
+console.log('myVarType ${myVarType}');
+
+
+    function runNow()
+    {
+        if(myVarType === 'number')
+            {
+                console.log('will 11 this one run?');
+            }
+            else
+            {
+                console.log('will 13 this one run?');
+            }
+    }
+
+runNow();
+runNow();
