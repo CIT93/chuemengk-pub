2d13e2642f99c8dbdb692895ab3d613a366cd26b
Chuemeng Kue <chuek125@gmail.com>
2024-09-06 07:26:07 -0700

    setup and display done

--- w3/main.js
+++ w3/main.js
@@ -21,10 +21,43 @@ if(householdSize === "Large")
 
                         }
 
+
+function determineHouseholdPoints(numberInHousehold) {
+    if(numberInHousehold === 1)
+        {
+            carbonFootPrintPoints = carbonFootPrintPoints + 14;
+        }
+    else if(numberInHousehold === 2)
+        {
+            carbonFootPrintPoints = carbonFootPrintPoints + 12;
+        }
+        else if(numberInHousehold === 3)
+        {
+            carbonFootPrintPoints = carbonFootPrintPoints + 10;
+        }
+        else if(numberInHousehold === 4)
+        {
+            carbonFootPrintPoints = carbonFootPrintPoints + 8;
+        }
+        else if(numberInHousehold === 5)
+        {
+            carbonFootPrintPoints = carbonFootPrintPoints + 6;
+        }
+        else if(numberInHousehold === 6)
+        {
+            carbonFootPrintPoints = carbonFootPrintPoints + 4;
+        }
+        else if(numberInHousehold > 6)
+        {
+            carbonFootPrintPoints = carbonFootPrintPoints + 2;
+        }
+        console.log(`Based on the number of the household which is ${numberInHousehold} the carbon footprint would be ${carbonFootPrintPoints}.`);
+                        
+        }
 let carbonFootPrintPoints = 0;
 //const householdSize;
 
 //global scope space 
 
-determineHouseholdPoints("Small");
+determineHouseholdPoints(3);
 determineHouseholdPoints("Large");
